680e3af638a870af5ee367fa7f6a144a205e2b5a
Michael Rider <mikepdxrider@gmail.com>
2021-09-20 17:21:45 -0700

    adds ingredients data

---
+++ src/ingredient-data.js
@@ -0,0 +1,38 @@
+export const ingredientsArr = [
+    {
+        quantity: '1 1/2 cups', ingredient: 'Milk'
+    },
+    {
+        quantity: '1/2 cup', ingredient: 'Mascarpone'
+    },
+    {
+        quantity: '1/2 tsp', ingredient: 'Pink Salt'
+    },
+    {
+        quantity: '1 lb', ingredient: 'Black Mission Figs'
+    },
+    {
+        quantity: '1/2 cup', ingredient: 'Brown Sugar'
+    },
+    {
+        quantity: '2-4 tbsp water', ingredient: 'Water'
+    },
+    {
+        quantity: '1 1/2 cups', ingredient: 'Heavy Cream'
+    },
+    {
+        quantity: '1/3 cup', ingredient: 'Granulated Sugar'
+    },
+    {
+        quantity: '2', ingredient: 'Egg Yolk'
+    },
+    {
+        quantity: '1', ingredient: 'Lemon, juiced'
+    },
+    {
+        quantity: '2 Tbsp', ingredient: 'Butter'
+    },
+    {
+        quantity: '1 cup', ingredient: 'Honey Roasted Pecans, roughly chopped'
+    },
+]
--- src/util-components/PComponent.js
+++ src/util-components/PComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class PComponent extends Component {
     render() {
         return (
-            <p>
+            <p className={this.props.classData}>
                 {this.props.textData}
             </p>
         )
