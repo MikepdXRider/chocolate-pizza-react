8c7234e0519b6242dd1380cc5b1e33f92634a62b
Michael Rider <mikepdxrider@gmail.com>
2021-09-20 17:58:40 -0700

    adds FooterComponent

---
+++ src/footer-components/ButtonComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ButtonComponent extends Component {
+    render() {
+        return (
+            <button>
+                SHARE RECIPE
+            </button>
+        )
+    }
+}
